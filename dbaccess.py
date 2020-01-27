790a8a1ad32a4d7647c7a7c0284155e4f9985bc0
ManuelHacker <manuel.hacker@sbg.at>
2020-01-27 20:21:14 +0100

    add assign_artist_to_user function

--- dbaccess.py
+++ dbaccess.py
@@ -47,3 +47,32 @@ def get_artist_id_by_name(conn, artists):
     ids = cursor.fetchall()
     cursor.close()
     return ids
+
+
+def assign_artist_to_user(conn, artists, user, timerange_string):
+    """Delete the users artists in the given time range and assign the new ones"""
+    cursor = conn.cursor()
+    user_id = get_user_id(conn, user)
+    timerange_int = switch_timerange(timerange_string)
+    cursor.execute("DELETE FROM user_has_artist WHERE timerange = ? AND user_id = ?", (timerange_int, user_id,))
+    conn.commit()
+
+    artist_id_array = get_artist_id_by_name(conn, artists)
+    # map the artist id array and push it to the insert data array
+    prepared_data = []
+    for id in artist_id_array:
+        prepared_data.append((None, timerange_int, id[0], user_id))
+
+    cursor.executemany("INSERT INTO user_has_artist VALUES (?, ?, ?, ?)", prepared_data)
+    conn.commit()
+    cursor.close()
+
+
+def switch_timerange(timerange_string):
+    """return the defined int number for the given timerange_string"""
+    switcher = {
+        "long_term": 1,
+        "short_term": 2,
+        "mid_term": 3
+    }
+    return switcher.get(timerange_string)
