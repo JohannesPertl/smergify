c9843359c36f10da43cdc5d8dcfdb29e291e4df8
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-01-29 16:50:47 +0100

    Basic code for smergify on server

--- server/entities/group.py
+++ server/entities/group.py
@@ -1,7 +1,8 @@
 class Group:
-    def __init__(self, group_name, group_id=None):
+    def __init__(self, group_name, group_id=None, users=None):
         self.group_name = group_name
         self.group_id = group_id
+        self.users = users
 
     def to_tuple(self):
-        return self.group_name
+        return self.group_name, self.group_id, self.users
