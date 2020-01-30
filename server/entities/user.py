a44598e565b4d7e8ea226cd0d59111676bc3c5df
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-01-30 23:23:16 +0100

    Rename spotify_token to token

--- server/entities/user.py
+++ server/entities/user.py
@@ -1,11 +1,11 @@
 class User:
-    def __init__(self, user_name, user_id, spotify_token, user_group=None, artists=None):
+    def __init__(self, user_name, user_id, token, user_group=None, artists=None):
         if artists is None:
             artists = list()
         self.artists = artists
         self.user_name = user_name
         self.user_id = user_id
-        self.user_token = spotify_token
+        self.token = token
         self.user_group = user_group
 
     def to_tuple(self):
