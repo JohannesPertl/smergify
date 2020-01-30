814fe1b6fc629e3d0593fb82387487175790b19e
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-01-30 15:33:55 +0100

    Remove first_login

--- server/entities/user.py
+++ server/entities/user.py
@@ -1,9 +1,8 @@
 class User:
-    def __init__(self, user_name, user_id, first_login, user_group=None,):
+    def __init__(self, user_name, user_id, user_group=None, ):
         self.user_id = user_id
         self.user_name = user_name
-        self.first_login = first_login
         self.user_group = user_group
 
     def to_tuple(self):
-        return self.user_id, self.user_name, self.first_login
+        return self.user_id, self.user_name
