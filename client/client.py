c462e78613b40a378d9c15bec4f3d754b59b11e8
Liliana Olczak <liliana.olczak@edu.fh-joanneum.at>
2020-01-30 22:28:00 +0100

    Implement create_group_folder_serverside() Implement transfer_file() Optimize code

--- client/client.py
+++ client/client.py
@@ -1,26 +1,13 @@
 import paramiko
 import yaml
-import spotipy
 import spotipy.util as util
 
+# get credentials from yaml
 with open("credentials.yaml") as config_file:
     CONFIG = yaml.safe_load(config_file)
 
+# permissions for spotify app
 SCOPE = "user-top-read"
-
-
-def user_authentication(user_name):
-    # Spotify Auth + .cache File Generierung:
-    token = util.prompt_for_user_token(
-        username=user_name,
-        scope=SCOPE,
-        client_id=CONFIG["app_id"],
-        client_secret=CONFIG["app_secret"],
-        redirect_uri=CONFIG["redirect_uri"],
-        show_dialog=True
-    )
-    if not token:
-        user_authentication()
 
 
 def main():
@@ -39,9 +26,38 @@ def main():
         password=CONFIG["password"]
     )
 
-    # TODO: transfer cache file to server (target: folder named like group). If folder already exists...
+    cache_file = ".cache-" + user_name
+    server_path = CONFIG["server-path"]
 
+    # FTP client:
+    ftp_client = ssh.open_sftp()
+
+    # List files on server:
+    dirs = ftp_client.listdir(server_path)
+
+    # check + create directory for group:
+    create_group_folder_serverside(dirs, server_path, user_group, ftp_client)
+
+    # File transfer to server:
+    target_path = server_path + user_group + "/" + cache_file
+    transfer_file(ftp_client, cache_file, target_path)
+
+    ftp_client.close()
     ssh.close()
+
+
+# Spotify authentication + cache-file generation:
+def user_authentication(user_name):
+    token = util.prompt_for_user_token(
+        username=user_name,
+        scope=SCOPE,
+        client_id=CONFIG["app-id"],
+        client_secret=CONFIG["app-secret"],
+        redirect_uri=CONFIG["redirect-uri"],
+        show_dialog=True
+    )
+    if not token:
+        user_authentication(user_name)
 
 
 def create_ssh_connection(username, hostname, port=22, rsa_key=None, password=None):
@@ -57,6 +73,15 @@ def create_ssh_connection(username, hostname, port=22, rsa_key=None, password=No
     )
 
     return ssh
+
+
+def transfer_file(ftp_client, source_path, target_path):
+    ftp_client.put(source_path, target_path)
+
+
+def create_group_folder_serverside(dirs, path, group_name, ftp_client):
+    if group_name not in dirs:
+        ftp_client.mkdir(path + group_name)
 
 
 if __name__ == "__main__":
