cb2782f91c0002a2774d1d654547d69f795ff161
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-01-30 22:08:02 +0100

    Implement checking for arguments passed to the script Implement creating groups Implement creating users for groups

--- server/smergify.py
+++ server/smergify.py
@@ -1,67 +1,88 @@
+import logging
+import os
+import re
+import sys
+import yaml
 import spotipy
 from spotipy import util
-import yaml
-import sys
-import os
-from server.entities import User
-from server.entities import Group
-from server.entities import Artist
-from server.entities import Song
+from server.entities import Group, User
 
 with open("config.yaml") as config_file:
     CONFIG = yaml.safe_load(config_file)
 
+# Constants
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+GROUPS_PATH = os.path.join(ROOT_PATH, "user_groups")
+SCOPE = "user-top-read"
 
 
-def check_arguments():
-    if len(sys.argv) >= 1:
-        group_folder = sys.argv[1]
-        print(group_folder)
+def arguments_given():
+    return len(sys.argv) > 1
+
+
+def create_groups_from_arguments():
+    groups = list()
+    for name in sys.argv:
+        groups.append(Group(name))
+    return groups
+
+
+def create_groups_from_path(path):
+    groups = list()
+    for f in os.scandir(path):
+        if f.is_dir():
+            groups.append(Group(f.name))
+    return groups
+
+
+def create_users_for_groups(groups):
+    users_to_return = list()
+    for g in groups:
+        group_path = os.path.join(GROUPS_PATH, g.group_name)
+        for file in os.scandir(group_path):
+            if file.is_file() and file.name.startswith(".cache-"):
+                user_name = re.sub(r"\.cache-", "", file.name)  # Read username from cache file by cutting .cache-
+
+                cache_path = os.path.join(group_path, file.name)  # build cache path
+
+                spotify_token = authenticate_user(user_name, cache_path)
+                user_id = get_user_id_from_spotify(spotify_token)
+                user = User(user_name, user_id, spotify_token, g)
+                g.users.append(user)
+                users_to_return.append(user)
+    return users_to_return
+
+
+def get_user_id_from_spotify(token):
+    spotify = spotipy.Spotify(auth=token)
+    user = spotify.current_user()
+    return user["uri"]
+
+
+def authenticate_user(user_name, cache_path):
+    token = util.prompt_for_user_token(
+        username=user_name,
+        scope=SCOPE,
+        client_id=CONFIG["app_id"],
+        client_secret=CONFIG["app_secret"],
+        redirect_uri=CONFIG["redirect_uri"],
+        show_dialog=True,
+        cache_path=cache_path
+    )
+    if not token:
+        logging.critical(f"File for user authentication named .cache-{user_name} does not exist! Exiting..")
+        sys.exit(1)
+
+    return token
 
 
 def main():
-    for subdir, dirs, files in os.walk(ROOT_PATH + "/user_groups"):
-        for file in files:
-            # print os.path.join(subdir, file)
-            filepath = subdir + os.sep + file
+    if arguments_given():
+        groups = create_groups_from_arguments()
+    else:
+        groups = create_groups_from_path(GROUPS_PATH)
 
-            print(subdir)
-            print(filepath)
-
-    # for username in user_list:
-    #     # Authenticate via existing cache file or in browser and then paste the uri
-    #     token = util.prompt_for_user_token(
-    #         username, CONFIG['scope'],
-    #         client_id=CONFIG['app_id'],
-    #         client_secret=CONFIG['app_secret'],
-    #         redirect_uri='http://localhost',  # TODO: Use smergify.duckdns.org with simple index.html apache server
-    #         show_dialog=True
-    #     )
-    #
-    #     if token:
-    #         spotify = spotipy.Spotify(auth=token)
-    #         # Top artists of user, possible time_range: short_term, medium_term, long_term
-    #         artists = (spotify.current_user_top_artists(limit=50, offset=0, time_range='long_term'))['items']
-    #         print(artists)
-    #         artist_names = list(map((lambda artist: artist['name']), artists))
-    #         # TODO: More variables for artist
-    #             # * URI for artist_top_tracks
-    #
-    #
-    #         print(spotify.artist_top_tracks("spotify:artist:0GDGKpJFhVpcjIGF8N6Ewt"))
-    #         # TODO: Remove
-    #         # for name in artist_names:
-    #             # print(name)
-    #
-    #         # TODO: Merge artists into combined lists (differences, matches)
-    #
-    #         # TODO: Create playlist of combined artists (use artist_top_tracks for tracks)
-    #         # TODO: 1. Playlist of matches
-    #         #       2. Playlist of differences
-    #
-    #         # Implement other functions to generate playlist
-    #             # TODO: Read lable from artist, create playlist based on lable
+    users = create_users_for_groups(groups)
 
 
 if __name__ == "__main__":
