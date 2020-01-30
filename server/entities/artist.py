e4f63604423f4a4e960d6bbb543886358090bb69
Johannes Pertl <johannes.pertl@edu.fh-joanneum.at>
2020-01-30 22:37:40 +0100

    Add time_range to artist

--- server/entities/artist.py
+++ server/entities/artist.py
@@ -1,9 +1,10 @@
 class Artist:
-    def __init__(self, artist_id, artist_name, songs=None):
+    def __init__(self, artist_id, artist_name, time_range, songs=None):
         if songs is None:
             songs = list()
         self.artist_id = artist_id
         self.artist_name = artist_name
+        self.time_range = time_range
         self.songs = songs
 
     def to_tuple(self):
